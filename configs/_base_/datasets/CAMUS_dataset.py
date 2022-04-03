18c0ab89aadb1844bdcb73bb5ab5f0d8fba05243
ys830 <1811326@mail.nankai.edu.cn>
2022-04-03 08:48:15 +0000

    “取消数据的repeat，添加CAMUS数据集用作测试，按iter跑”

--- configs/_base_/datasets/CAMUS_dataset.py
+++ configs/_base_/datasets/CAMUS_dataset.py
@@ -1,27 +1,27 @@
-dataset_type = 'renewCardiac2'
-data_root = './data/fix_tiff_cardiacdataset'
+dataset_type = 'Cardiac'
+data_root = './data/CAMUS_TEST'
 img_norm_cfg = dict(
-    mean=[32.631, 32.587, 32.570],
-    std=[49.875, 49.819, 49.779],
+    mean=[33.143],
+    std=[49.704],
     to_rgb=False)
 # img_scale = (960, 999)
-img_scale = (112, 112)
+img_scale = (748, 1231)
 # crop_size = (128, 128)
 crop_size = (112, 112)
-train_tif_pipeline = [
-    dict(type='LoadTIFImageFromFile'),
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
     dict(type='Resize', img_scale=img_scale, ratio_range=(0.5, 2.0)),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
-    dict(type='RandomFlip', prob=0),
+    dict(type='RandomFlip', prob=0.5),
     # dict(type='PhotoMetricDistortion'),
-    dict(type='TiffNormalize', **img_norm_cfg),
+    dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_semantic_seg'])
 ]
-test_tif_pipeline = [
-    dict(type='LoadTIFImageFromFile'),
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
         img_scale=img_scale,
@@ -30,7 +30,7 @@ test_tif_pipeline = [
         transforms=[
             dict(type='Resize', keep_ratio=True),
             dict(type='RandomFlip'),
-            dict(type='TiffNormalize', **img_norm_cfg),
+            dict(type='Normalize', **img_norm_cfg),
             dict(type='ImageToTensor', keys=['img']),
             dict(type='Collect', keys=['img'])
         ])
@@ -41,22 +41,22 @@ data = dict(
     workers_per_gpu=4,
     train=dict(
         type='RepeatDataset',
-        times=1,
+        times=40000,
         dataset=dict(
             type=dataset_type,
             data_root=data_root,
             img_dir='img_dir/TRAIN',
             ann_dir='ann_dir/TRAIN',
-            pipeline=train_tif_pipeline)),
+            pipeline=train_pipeline)),
     val=dict(
         type=dataset_type,
         data_root=data_root,
         img_dir='img_dir/VAL',
         ann_dir='ann_dir/VAL',
-        pipeline=test_tif_pipeline),
+        pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
         data_root=data_root,
         img_dir='img_dir/TEST',
         ann_dir='ann_dir/TEST',
-        pipeline=test_tif_pipeline))
+        pipeline=test_pipeline))
--- configs/_base_/schedules/schedule_40k.py
+++ configs/_base_/schedules/schedule_40k.py
@@ -4,6 +4,6 @@ optimizer_config = dict()
 # learning policy
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
 # runtime settings
-runner = dict(type='EpochBasedRunner', max_epochs=100)
-checkpoint_config = dict(by_epoch=True, interval=1)
-evaluation = dict(interval=1, metric='mIoU', pre_eval=True)
+runner = dict(type='IterBasedRunner', max_iters=40000)
+checkpoint_config = dict(by_epoch=False, interval=4000)
+evaluation = dict(interval=4000, metric='mIoU', pre_eval=True)
